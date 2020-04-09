e63b0352d27056ec2944fe47d0a51b076715675f
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-04-10 00:08:02 +0530

    vendor: Allow providing camera server and service
    Change-Id: Icb62bbd394719d2a0a122d6ce9364af1ae0c241f

--- config/BoardConfigKangOS.mk
+++ config/BoardConfigKangOS.mk
@@ -25,3 +25,9 @@ include vendor/kangos/config/BoardConfigQcom.mk
 endif
 
 include vendor/kangos/config/BoardConfigSoong.mk
+
+ifneq ($(TARGET_USES_PREBUILT_CAMERA_SERVICE), true)
+PRODUCT_SOONG_NAMESPACES += \
+    frameworks/av/camera/cameraserver \
+    frameworks/av/services/camera/libcameraservice
+endif
