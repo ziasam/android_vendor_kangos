RevengeOS/android_vendor_revengeos

copy backuptool files to system
For some reason in android11 it fails to copy to $OUT/install, so copy to system as a temporary workaround till the root cause is discovered

Change-Id: I14f8d38436a955f4fe8f491cfe7be28b58a2984e

config/common.mk
@@ -36,8 +36,8 @@ PRODUCT_COPY_FILES += \
 endif
 
 PRODUCT_COPY_FILES += \
-    vendor/revengeos/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/revengeos/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/revengeos/prebuilt/common/bin/backuptool.sh:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.sh \
+    vendor/revengeos/prebuilt/common/bin/backuptool.functions:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.functions \
     vendor/revengeos/prebuilt/common/bin/50-cm.sh:system/addon.d/50-cm.sh
 
 # Copy all custom init rc files
@@ -46,7 +46,7 @@ $(foreach f,$(wildcard vendor/revengeos/prebuilt/common/etc/init/*.rc),\
 
 # system mount
 PRODUCT_COPY_FILES += \
-    vendor/revengeos/prebuilt/common/bin/system-mount.sh:install/bin/system-mount.sh
+    vendor/revengeos/prebuilt/common/bin/system-mount.sh:$(TARGET_COPY_OUT_SYSTEM)/install/bin/system-mount.sh
 
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false

ziasam/android_vendor_kangos

config/common.mk
@@ -65,8 +65,8 @@ PRODUCT_COPY_FILES += \
 
 # KangOS-specific
 PRODUCT_COPY_FILES += \
-    vendor/kangos/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/kangos/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/kangos/prebuilt/common/bin/backuptool.sh:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.sh \
+    vendor/kangos/prebuilt/common/bin/backuptool.functions:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.functions \
     vendor/kangos/prebuilt/common/bin/50-cm.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-cm.sh
 
 # Copy all custom init rc files
@@ -75,7 +75,7 @@ $(foreach f,$(wildcard vendor/kangos/prebuilt/common/etc/init/*.rc),\
 
 # system mount
 PRODUCT_COPY_FILES += \
-    vendor/kangos/prebuilt/common/bin/system-mount.sh:install/bin/system-mount.sh
+    vendor/kangos/prebuilt/common/bin/system-mount.sh:$(TARGET_COPY_OUT_SYSTEM)/install/bin/system-mount.sh
 
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \